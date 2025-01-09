5b34c7e3abc0abcd243022c4eaab2c1727baf0e6
Shayan <nwryshayan500@gmail.com>
2025-01-09 16:20:04 +0330

    Sanity Insanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,20 @@
-import SearchForm from "../components/SearchForm";
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams}: {searchParams: Promise<{ query?: string }>}) {
   const query = (await searchParams).query
+
+  const posts = [
+    { 
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: 'Shayan' },
+      _id: 1,
+      description: 'this is a description',
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO41ENFecNfc1jCtnFo5dfW7pxtefpKnvagw&s",
+      category: 'Vroom Vroom',
+      title: "Super Cars"
+    }]
 
   return (
     <>
@@ -13,6 +26,22 @@ export default async function Home({ searchParams}: {searchParams: Promise<{ que
         </p>
 
         <SearchForm query={query}/>
+      </section>
+
+      <section className='section_container'>
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-result">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
